aa2c9b7fda63fe525f8336f740f4cbfaece73457
cristianku <c.zantedeschi@gmail.com>
2017-06-20 13:25:09 +0200

    *

--- Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -37,35 +37,47 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 # rescaling between 0 and 1 + transformations ## many different batches with RANDOM shifts, rotations, etc....
-train_datagen = ImageDataGenerator(
+train_datagen   = ImageDataGenerator(
                                     rescale=1. / 255,
                                     shear_range=0.2, #geometrical transformation
                                     zoom_range=0.2,
                                     horizontal_flip=True)
 
 # batches creation
-training_set = train_datagen.flow_from_directory('datasetsmall/training_set',
-                                                target_size=(64, 64),
-                                                batch_size=32,
-                                                class_mode='binary')
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size=(64, 64),  # this dimension should be same as Convolution Layer
+                                                                        # classifier.add(Convolution2D(32,3,3,
+                                                                        # input_shape=(64,64,3), activation='relu'))
+                                                 batch_size=32,
+                                                 class_mode='binary')
 
 # TEST SET AUGMENTATION
 
 # rescaling between 0 and 1
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen    = ImageDataGenerator(rescale=1. / 255)
 
 # batches creation
-test_set = test_datagen.flow_from_directory('datasetsmall/test_set',
-                                            target_size=(64, 64),
-                                            batch_size=32,
-                                            class_mode='binary')
-
+test_set        = test_datagen.flow_from_directory('dataset/test_set',
+                                                    target_size=(64, 64),
+                                                    batch_size=32,
+                                                    class_mode='binary')
+# Fitting the Model
+#
 classifier.fit_generator(
                     training_set,
-                    steps_per_epoch=80,
-                    epochs=10,
-                    validation_data=test_set,
-                    validation_steps=20)
+                    steps_per_epoch=25,      # number of images per epochs ( per batch )
+                    epochs=3,                #Total number of steps (batches of samples)
+                                              # to yield from generator before declaring one epoch
+                                              # finished and starting the next epoch.
+                                              # It should typically be equal to the number of unique samples
+                                              # of your dataset divided by the batch size.
+
+                    validation_data=test_set, #validation_data: This can be either
+                                              #  A generator
+                                              # for the validation data
+                                              # A tuple (inputs, targets)
+                                              # A tuple (inputs, targets, sample_weights).
+                    validation_steps=20)       # number of images per epochs ( per batch )
 
 
 
